ccdd9d8e0b65cd02727d0aa3cc5335b9ec5c987a
Nupur <nupursogani3101@gmail.com>
2021-06-03 01:40:26 +0530

    Gross Salary & distance conversion

--- CodingPractice/Day1.py
+++ CodingPractice/Day1.py
@@ -11,3 +11,20 @@ dearanceAllowance = 0.40
 houseRentAllowance = 0.20
 GrossSal = basicSal + (dearanceAllowance*basicSal) + (houseRentAllowance*basicSal)
 print("Gross Salary : ",GrossSal)
+
+
+"""
+Coding question 2:💻
+The distance between two cities (in km) is input through keyboard. Write a program to
+convert and print the distance in meters, feet, inches and centimetres.
+"""
+
+kmDist = int(input("Enter distance between two cities (in km): "))
+mDist = kmDist*1000
+cmDist = mDist*100
+ftDist = kmDist*3280.84
+inchDist = ftDist*12
+print(f"\nDistance in meters {':':>8} {mDist} m\
+\nDistance in feet {':':>10} {ftDist} ft\
+\nDistance in inches {':':>8} {inchDist} in\
+\nDistance in centimeters {':':>3} {cmDist} cm")
